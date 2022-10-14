95e8e17924a87b5aff7bd1a2622abb851ac2cecc
AndresLeonJ <105888687+AndresLeonJ@users.noreply.github.com>
2022-10-13 18:20:57 -0600

    Clase Persona

---
+++ CazaTalentos/src/cazatalentos/Persona.java
@@ -0,0 +1,41 @@
+package cazatalentos;
+
+public class Persona {
+    
+    public String nombre;
+    public String apellidos;
+    public int edad;
+    
+    public Persona(){
+        this.nombre="";
+        this.apellidos="";
+        this.edad=0;
+    }
+    //añadimos los getters y los setters 
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    
+    
+}
