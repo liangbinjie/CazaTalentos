911ac6b8f8c060c3fbd7f1a1fd87dba93660ec96
liangbinjie <mailandresliang@gmail.com>
2022-10-15 16:13:32 -0600

    Update Usuarios.java
    El LinkedList de usuarios se usara en otra clase
    Heredamos el nombre y apellido del archivo persona

--- CazaTalentos/src/cazatalentos/Usuarios.java
+++ CazaTalentos/src/cazatalentos/Usuarios.java
@@ -5,52 +5,58 @@ import java.util.LinkedList;
  *
  * @author Binjie Liang
  */
-public class Usuarios {
+public class Usuarios extends Persona {
     
-    public LinkedList usuarios = new LinkedList<>();
+//    public LinkedList usuarios = new LinkedList<>();
     
     
-    private String nombre;
-    private String apellidos;
+    // private String nombre;
+    // private String apellidos;
     private String usuario;
     private String password;
     private Estados estado;
     
     // constructor 
     public Usuarios() {
-        this.nombre = "";
-        this.apellidos = "";
+        super();
         this.usuario = "";
         this.password = "";
         this.estado = Estados.Inactivo;
+    }
+
+    public Usuarios(String nombre, String apellidos, String usuario, String password) {
+        // super(params);
+        this.usuario = usuario;
+        this.password = password;
+        this.estado = Estados.Activo;
     }
     
     
     // getters and setters
 
-    public LinkedList getUsuarios() {
-        return usuarios;
-    }
+//    public LinkedList getUsuarios() {
+//        return usuarios;
+//    }
+//
+//    public void setUsuarios(LinkedList usuarios) {
+//        this.usuarios = usuarios;
+//    }
 
-    public void setUsuarios(LinkedList usuarios) {
-        this.usuarios = usuarios;
-    }
+    // public String getNombre() {
+    //     return nombre;
+    // }
 
-    public String getNombre() {
-        return nombre;
-    }
+    // public void setNombre(String nombre) {
+    //     this.nombre = nombre;
+    // }
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
+    // public String getApellidos() {
+    //     return apellidos;
+    // }
 
-    public String getApellidos() {
-        return apellidos;
-    }
-
-    public void setApellidos(String apellidos) {
-        this.apellidos = apellidos;
-    }
+    // public void setApellidos(String apellidos) {
+    //     this.apellidos = apellidos;
+    // }
 
     public String getUsuario() {
         return usuario;
