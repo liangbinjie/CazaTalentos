d71e8c0bf8dba1d40d95b222cea8086b54044aaa
liangbinjie <mailandresliang@gmail.com>
2022-11-30 15:42:32 -0600

    Create Padres.java

---
+++ CazaTalentos/src/cazatalentos/Padres.java
@@ -0,0 +1,88 @@
+package cazatalentos;
+
+import java.util.ArrayList;
+
+public class Padres extends Persona {
+    private ArrayList<Long> hijos = new ArrayList<>();
+    
+    public Padres() {
+        super();
+        this.hijos = null;
+    }
+
+    public ArrayList<Long> getHijos() {
+        return hijos;
+    }
+
+    public void setHijos(ArrayList<Long> hijos) {
+        this.hijos = hijos;
+    }
+
+    @Override
+    public String getNombre() {
+        return getNombre();
+    }
+
+
+    public void setNombre(String nombre) {
+        this.setNombre(nombre);
+    }
+
+    public String getApellidos() {
+        return getApellidos();
+    }
+
+    public void setApellidos(String apellidos) {
+        this.setApellidos(apellidos);
+    }
+
+    public int getEdad() {
+        return getEdad();
+    }
+
+    public void setEdad(int edad) {
+        this.setEdad(edad);
+    }
+
+    public long getIdentificacion() {
+        return getIdentificacion();
+    }
+
+    public void setIdentificacion(long identificacion) {
+        this.setIdentificacion(identificacion);
+    }
+
+    public long getTelefono() {
+        return getTelefono();
+    }
+
+    public void setTelefono(long telefono) {
+        this.setTelefono(telefono);
+    }
+
+    public String getCiudad() {
+        return getCiudad();
+    }
+
+    public void setCiudad(String ciudad) {
+        this.setCiudad(ciudad);
+    }
+
+    public String getDireccion() {
+        return getDireccion();
+    }
+
+    public void setDireccion(String direccion) {
+        this.setDireccion(direccion);
+    }
+
+    public String getEmail() {
+        return getEmail();
+    }
+
+    public void setEmail(String email) {
+        this.setEmail(email);
+    }
+    
+    
+}
