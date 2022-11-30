881fc6e9830a34d482299539aebdaae60c0ac0cb
liangbinjie <mailandresliang@gmail.com>
2022-11-30 15:42:04 -0600

    Usuarios

---
+++ CazaTalentos/src/cazatalentos/Usuarios.java
@@ -0,0 +1,82 @@
+package cazatalentos;
+
+public class Usuarios {
+    private int id;
+    private String nombre;
+    private String apellidos;
+    private String username;
+    private String password;
+    private String role;
+    private boolean estado;
+    
+    public Usuarios() {
+        this.id = 0;
+        this.nombre = "";
+        this.apellidos = "";
+        this.username = "";
+        this.password = "";
+        this.role = "";
+        this.estado = false;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+    
+    
+    
+    
+    
+}
